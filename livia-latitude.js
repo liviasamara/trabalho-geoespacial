5a4bb6ea53a8479a4703a80f0b4e4b1f52211e1c
Livia Samara <liviacardoso725@gmail.com>
2017-10-27 15:38:07 -0200

    trab

---
+++ livia-latitude.js
@@ -0,0 +1,173 @@
+bd.alunos.insert({
+	name:"Luana  A. Ramos",
+	location:{type:"Point", coordinates:[-15.8052074,-43.3160809]}
+]}
+
+bd.alunos.insert({
+	name:"Joana D'arc",
+	location:{type:"Point", coordinates:[-15.7943479,-43.310403]}
+]}
+
+bd.alunos.insert({
+	name:"Alberto Souza Cunha",
+	location:{type:"Point", coordinates:[-158057438,-43.3179225]}
+]}
+
+bd.alunos.insert({
+	name:"Layla Teodora",
+	location:{type:"Point", coordinates:[-15.8198619,-43.3179687]}
+]}
+
+bd.alunos.insert({
+	name:"Thalia Santos Cunha",
+	location:{type:"Point", coordinates:[-15.792172,-43.341919]}
+]}
+
+bd.alunos.insert({
+	name:"Emilly Raissa M. Rodr gius",
+	location:{type:"Point", coordinates:[-15.807591,-43.312611]}
+]}
+
+bd.alunos.insert({
+	name:"Sávia Fernandes  Marques ",
+	location:{type:"Point", coordinates:[-15.7814025,-43.3223997]}
+]}
+
+bd.alunos.insert({
+	name:"Dieyse Michele N. Leal",
+	location:{type:"Point", coordinates:[-15.781380,-43.323578]}
+]}
+
+bd.alunos.insert({
+	name:"Vitoria  Ingred de Oliveira",
+	location:{type:"Point", coordinates:[-15.817489,-43.314893]}
+]}
+
+bd.alunos.insert({
+	name:"joão Vitor  Silva Matos",
+	location:{type:"Point", coordinates:[-15.8147075,-43.306551]}
+]}
+
+
+********************
+
+bd.alunos.insert({
+	name:"Michelle de Oliveira  Santos",
+	location:{type:"Point", coordinates:[-15.8263155,-43.3123914]}
+]}
+
+bd.alunos.insert({
+	name:"Maria Thais Rodrigues Silva",
+	location:{type:"Point", coordinates:[-15.7377377,-43.0288791]}
+]}
+
+bd.alunos.insert({
+	name:"Ádrian  Marcello  Mendes Dias",
+	location:{type:"Point", coordinates:[-15.811190,-43.304557]}
+]}
+
+bd.alunos.insert({
+	name:"Lara Mariana  G. Pereira",
+	location:{type:"Point", coordinates:[-15.8197566,-43.3153666]}
+]}
+
+bd.alunos.insert({
+	name:"Camila Silva Alves ",
+	location:{type:"Point", coordinates:[-15.8251198,-43.2960149]}
+]}
+
+bd.alunos.insert({
+	name:"Lívia Samara Cardoso Gonçalves",
+	location:{type:"Point", coordinates:[-15.7953561,-43.314748]}
+]}
+
+bd.alunos.insert({
+	name:"Micaelle Alves  Martins",
+	location:{type:"Point", coordinates:[-15.8221044,-43.3148092]}
+]}
+
+bd.alunos.insert({
+	name:"Flora Mendes",
+	location:{type:"Point", coordinates:[-15.8018395,-43.3051085]}
+]}
+
+bd.alunos.insert({
+	name:"Jade Passos Almeida",
+	location:{type:"Point", coordinates:[-15.819087,-43.309728]}
+]}
+
+bd.alunos.insert({
+	name:"Sidney Emanuel",
+	location:{type:"Point", coordinates:[-15.80360014,-43.3121088]}
+]}
+
+***********
+
+bd.alunos.insert({
+	name:"Lavínia  Tamires  Souza Araújo",
+	location:{type:"Point", coordinates:[-15.8045297,-43.3121088]}
+]}
+
+bd.alunos.insert({
+	name:"Victória Felipe  D. Souza",
+	location:{type:"Point", coordinates:[-15.818761,-43.300751]}
+]}
+
+bd.alunos.insert({
+	name:"Emille Beatriz ",
+	location:{type:"Point", coordinates:[-15.8039344,-43.3125666]}
+]}
+
+bd.alunos.insert({
+	name:"Hellen Aline R. Silva",
+	location:{type:"Point", coordinates:[-15.7990549,-43.3165794]}
+]}
+
+bd.alunos.insert({
+	name:"Joyce Marianne",
+	location:{type:"Point", coordinates:[-15.8290764,-43.3087511]}
+]}
+
+bd.alunos.insert({
+	name:"Amanda Pereira Daniel",
+	location:{type:"Point", coordinates:[-15.802350,-43.314899]}
+]}
+
+bd.alunos.insert({
+	name:"Luan ALves Vieira",
+	location:{type:"Point", coordinates:[-15.8007558,-43.3138138]}
+]}
+
+bd.alunos.insert({
+	name:"João Gabriel De A. Silva",
+	location:{type:"Point", coordinates:[-15.8489078,-43.3049733]}
+]}
+
+bd.alunos.insert({
+	name:"Daniel Barbosa  Mendes ",
+	location:{type:"Point", coordinates:[-15.8025955,-43.3084923]}
+]}
+
+bd.alunos.insert({
+	name:"Tom  Ruas Costas",
+	location:{type:"Point", coordinates:[-15.802596,-43.7090244]}
+]}
+
+bd.alunos.insert({
+	name:"Mércia  Carroline",
+	location:{type:"Point", coordinates:[-15.5343928,-43.0685685]}
+]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
